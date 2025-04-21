c02a85273555af480de9e48762e548b7f0b5d31a
CatherineZ1001 <catherinez1001@CatherineZ.>
2025-04-21 11:49:22 -0400

    Q4

--- TrafficData.hpp
+++ TrafficData.hpp
@@ -7,9 +7,31 @@
 #include <sstream>
 
 std::map<std::string, int> loadTrafficData(const std::string& filename) {
-  //implement your function here!
+	std::map<std::string, int> data = {};
+
+	std::ifstream inFile(filename);
+	if (!inFile){
+		std::cerr << -1 << std::endl;
+		return;
+	}
+
+	std::string content;
+	while (std::getline(inFile, content)){
+		data[content.substr(0, 3)] = std::stoi(content.substr(4));
+	}
+
+	return data;
 }
 
 void updateTrafficData(const std::string& filename, std::map<std::string, int>& dataMap) {
-    //implement your function here!
+	std::ifstream inFile(filename);
+	if (!inFile){
+		std::cerr << -1 << std::endl;
+		return;
+	}
+
+	std::string content;
+	while (std::getline(inFile, content)){
+		dataMap[content.substr(0, 3)] = std::stoi(content.substr(4));
+	}
 }
